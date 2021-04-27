8ad568b435354855ce9a80edd1f5b649c00ea9f8
Jean Malm <jean.malm@mdh.se>
2021-04-27 16:35:23 +0200

    Replaced list with set (which requires adding hashing and equal to CodeChecker report)

--- scripts/my_plist_parser.py
+++ scripts/my_plist_parser.py
@@ -8,6 +8,10 @@ import re
 from typing import List, Dict, Tuple
 from codechecker_common.report import Report
 from enum import Enum
+from functools import partial
+
+import logging
+logging.basicConfig(filename='SPA_Comparison.log', filemode='w', format='%(asctime)s %(message)s')
 
 
 class DuplicateRelations(Enum):
@@ -22,12 +26,15 @@ def is_duplicate(w1: Report, w2: Report, col_distance=0, row_distance=0) -> Dupl
     # 2) Warning originates on the same line
     if abs(w1.line - w2.line) > row_distance:
         return DuplicateRelations.NO_DUPLICATE
-    if w1.check_name == w2.check_name:
-        return DuplicateRelations.SUBSUMPTION
+    elif abs(w1.col - w2.col) > col_distance:
+        return DuplicateRelations.NO_DUPLICATE
     else:
-        return DuplicateRelations.NO_DUPLICATE \
-            if abs(w1.col - w2.col) > col_distance \
-            else DuplicateRelations.POTENTIAL_DUPLICATE
+        if w1.check_name == w2.check_name and w1.description == w2.description:
+            return DuplicateRelations.SUBSUMPTION
+        else:
+            return DuplicateRelations.POTENTIAL_DUPLICATE
+
+
 
 
 def prettify(dir_name):
@@ -75,10 +82,13 @@ def collapse_reports_toolpair_list(reports_tool_list: List[Tuple[Report, str]]) 
 def get_duplicate_unique_list_pairs(report_tool_list: List[Tuple[Report, str]]) -> \
         Tuple[List[List[Tuple[Report, str]]], List[Tuple[Report, str]]]:
     """Takes a list of reports and groups them as duplicated reports or as a list of unique reports"""
-
+    def split_list(__list, func, predicate_result):
+        predicate_is_true = [e for e in __list if func(e[0]) == predicate_result]
+        predicate_is_false = [e for e in __list if func(e[0]) != predicate_result]
+        return predicate_is_true, predicate_is_false
 
     # Define inner recursive function that does the work
-
+    # TODO: May want to rewrite into loop, since appending to python lists is a mutable action anyways
     # TODO: Some optimisation potential. Subsumed tools need not be checked against each other
     #  and if there is only one tool left, we assume it's internally consistent and should not cause duplicates
     def inner(_l: List[Tuple[Report, str]],
@@ -91,16 +101,23 @@ def get_duplicate_unique_list_pairs(report_tool_list: List[Tuple[Report, str]]) 
         # List unpacking
         head, *tail = _l
 
+        match_head = partial(is_duplicate, head[0])
         # Split tail based on duplicate status
-        potential_duplicates = [e for e in tail if is_duplicate(head[0], e[0]) != DuplicateRelations.NO_DUPLICATE]
-        remainder = [e for e in tail if is_duplicate(head[0], e[0]) == DuplicateRelations.NO_DUPLICATE]
+        # potential_duplicates = [e for e in tail if is_duplicate(head[0], e[0]) == DuplicateRelations.POTENTIAL_DUPLICATE]
+        # remainder = [e for e in tail if is_duplicate(head[0], e[0]) == DuplicateRelations.NO_DUPLICATE]
+        # subsumed_reports = [e for e in tail if is_duplicate(head[0], e[0]) == DuplicateRelations.NO_DUPLICATE]
+        potential_duplicates, __rest = split_list(tail, match_head, DuplicateRelations.POTENTIAL_DUPLICATE)
+        remainder, subsumed = split_list(__rest, match_head, DuplicateRelations.NO_DUPLICATE)
 
+        if len(subsumed) > 0:
+            logging.info("head %s subsumes the following reports %s\n", str(head), str(subsumed))
         # If we found duplicates, there are no unique findings, hence uniques is forwarded unchanged
         if len(potential_duplicates) > 0:
             duplicates.append([head] + potential_duplicates)
         # Otherwise, head is a unique report
         else:
             uniques.append(head)
+        # Finally, we make the recursive call to keep processing
         return inner(remainder,
                      duplicates,
                      uniques)
@@ -124,17 +141,16 @@ def get_reports_per_file(plist_result_directories):
             for report in reports:
                 report_main_file = report.file_path
                 if report_main_file not in filename_reports_key_val:
-                    filename_reports_key_val[report_main_file] = []
-                # TODO: This causes many duplications due to several issues appearing in the same file.
-                # Consider changing this to a set instead of list
-                filename_reports_key_val[report_main_file].append((report, tool_run))
+                    filename_reports_key_val[report_main_file] = set()
+                # TODO: This causes many duplications due to several issues having the same "main" file
+                filename_reports_key_val[report_main_file].add((report, tool_run))
 
     filename_reports_key_val_duplicates = dict(map(lambda e: (e[0],
                                                               get_duplicate_unique_list_pairs(e[1])
                                                               ),
                                                    filename_reports_key_val.items()))
 
-    collapsed_reports_tool_pair = dict(map(lambda e: (e[0], collapse_reports_toolpair_list(e[1])),
+    collapsed_reports_tool_pair = dict(map(lambda e: (e[0], collapse_reports_toolpair_list(list(e[1]))),
                                            filename_reports_key_val.items()))
     return collapsed_reports_tool_pair, filename_reports_key_val_duplicates
 
