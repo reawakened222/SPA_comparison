43807738636bc1cb7596bdcd0952e760d0c0f8d2
Jean Malm <jean.malm@mdh.se>
2021-05-03 18:26:10 +0200

    Cloc filter integration in progress

--- scripts/github_api_utils.py
+++ scripts/github_api_utils.py
@@ -1,14 +1,24 @@
+import logging
+
 from github import Github
 import os
+import re
 import enum
 import argparse
 import json  # For serializing
 import requests  # For Rest API calls
 import copy
+import random
+import itertools
 from datetime import datetime
 from functools import partial
-
+import subprocess
+from dotenv import load_dotenv
+load_dotenv(".env")
 token = os.getenv('GITHUB_TOKEN', '...')
+CLOC_BIN = os.getenv("CLOC_BIN", '/usr/bin/cloc')
+
+logging.basicConfig(filename="GITHUB_LOG.log",format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
 
 class SearchParameter:
@@ -88,14 +98,62 @@ def get_projects_from_user(py_git, username):
     return py_git.get_user(username).get_repos()
 
 
-def filter_on_languages(language_size_pairs, repository):
-    """Filter based on the overall languages used"""
+# Filtering
+# 1) language (general)
+# 2) Language + size (test)
+# Note: Github's API gives size value in bytes, this is ~10MB of code in C/C++, Java or Python
+min_size_in_bytes = 10 * 1000 * 1000
+general_lang_sizes = [("C++", min_size_in_bytes),
+                       ("C", min_size_in_bytes),
+                       ("Java", min_size_in_bytes),
+                       ("Python", min_size_in_bytes)]
+# And this is low/upper bound for mid-size project
+midsize_project = (50000, 100000)
+LoC_lang_sizes_midsize_project = dict({("C++", midsize_project),
+                                       ("C", midsize_project),
+                                       ("Java", midsize_project),
+                                       ("Python", midsize_project)})
+
+
+def filter_on_languages(language_size_dict, repository):
+    """Filter based on the overall languages used, in number of bytes"""
     language_lookup = requests.get(repository.languages_url).json()
-    totalsize = sum(map(lambda e: int(e[1]), language_lookup.items()))
-    for l, size in language_lookup.items():
-        if l in language_size_pairs[0]:
+    repository_languages = language_lookup.items()
+    if not repository_languages:
+        return False
+
+    # total_size = sum(map(lambda e: int(e[1]), language_lookup.items()))
+
+    # This is a fixed length for each run
+    for requested_lang, lang_min_size in language_size_dict:
+        size_for_repo = repository_languages.get(requested_lang)
+        if size_for_repo and size_for_repo >= lang_min_size:
             return True
     return False
+
+# TODO: FIX THIS
+cloc_result_pattern = re.compile("\n((?:\S| )+?)\s*?(\d+)\s*?(\d+)\s*?(\d+)\s*?(\d+)")
+def cloc_invocation(languages, perl_dir_filter="", top_folder = "."):
+    directory_filter = " --match-d="+ perl_dir_filter + " " if perl_dir_filter != "" else " "
+    invocation = [CLOC_BIN, f'--include-lang={",".join(languages)}']
+    if directory_filter != " ":
+        invocation.append(directory_filter)
+    invocation.append(top_folder)
+    cloc_res = subprocess.run(invocation, capture_output=True)
+    if cloc_res.returncode == 0:
+        # Parse stdout
+        counted = str(cloc_res.stdout)
+        languages = counted.split("-------------------------------------------------------------------------------")[2]
+
+        matches = cloc_result_pattern.findall(languages)
+        if matches:
+            print(matches)
+
+
+
+    return cloc_res.returncode
+
+
 
 
 def filter_on_testware_language(languages, repository):
@@ -105,23 +163,46 @@ def filter_on_testware_language(languages, repository):
 
 
 def filter_on_project_loc_size(languages_and_minimal_size_pairs, repository):
+    """Filtering based on LoC"""
     """Based on
     https://stackoverflow.com/questions/26881441/can-you-get-the-number-of-lines-of-code-from-a-github-repository
     the simplest way seems to be to shallow clone it, run e.g. cloc and then parse results"""
 
 
 def apply_filters_log_filtering(repo_list, repository_filters):
-    """Given a list of repositories, we filter (with logging) based on the set of filters supplied"""
-    # Filtering
-    # 1) language (general)
-    # 2) Language + size (test)
+    """Given a list of repositories, we filter (with logging) based on the set of filters supplied.
+    Result will be a list of the list of projects that were left after each filter step"""
+
+    def inner(repos, filters, acc):
+        if not filters:
+            return acc
+        f_head, *f_tail = filters
+        filtered_repositories = [r for r in repos if f_head(r)] # filter(f_head, repos)
+        acc.append(filtered_repositories)
+        return inner(filtered_repositories, f_tail, acc)
+
+
+#    reduced_list = list(itertools.islice(repo_list, 100))
+#    return inner(reduced_list, repository_filters, [reduced_list])
+    # I want to keep each step of the filtering, will make it easier for data extraction
+    return inner(repo_list, repository_filters, [repo_list])
 
 
 def eval_example(github_user):
     g = Github(token)
     repos = get_projects_from_user(g, github_user)
 
-    #if repos:
+    if not repos:
+        logging.DEBUG("No repositories returned from user: " + github_user)
+        return None
+    # Partial applications of filter configuration on filter functions
+    # Should return a function for which the remaining argument is the repository object from pygit
+    filters = [partial(filter_on_languages, general_lang_sizes)]
+    filtered_projects = apply_filters_log_filtering(repos, filters)
+    with open("Filtered_Projects.log", "w") as out_file:
+        for r in filtered_projects[-1]:
+            out_file.write(r.__str__())
+            out_file.write("\n")
 
 
 
@@ -134,7 +215,7 @@ def make_clone_script():
                         default=['C', 'C++'], help="Provide Main languages to search for")
     parser.add_argument('-sp', '--searchphrase', help="Provide search string to use (will target readme file")
     args = parser.parse_args()
-    #python3 github_api_utils.py --size=100000 --stars=1000 --forks=100 --languages=["Java"] --searchphrase="test"
+    # python3 github_api_utils.py --size=100000 --stars=1000 --forks=100 --languages=["Java"] --searchphrase="test"
     search = SearchData(args.stars, args.forks, args.size, args.searchphrase, args.languages).toSearchString()
     now = datetime.now()
     dt_string = now.strftime("%d%m%y_%H_%M_%S")
@@ -159,5 +240,6 @@ def make_clone_script():
 
 
 if __name__ == "__main__":
-    #make_clone_script()
-    eval_example("Apache")
+    # make_clone_script()
+    #eval_example("Apache")
+    cloc_invocation(languages=["C", "C++", "\"C/C++ Header\"", "Python", "Java"], perl_dir_filter="", top_folder="/home/jmm01/Git_Repos/KAzNU/apac")
