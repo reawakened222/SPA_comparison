525056be9e8cfc4664a22d0ac41139bbe40edc81
Jean Malm <jean.malm@mdh.se>
2021-05-11 12:13:41 +0200

    Bug during refactoring, CodeChecker was called using relative path in subprocess. Refactored into constant variable

--- scripts/codechecker_interface.py
+++ scripts/codechecker_interface.py
@@ -7,12 +7,13 @@ CODECHECKER_PATH = f"{HOME}/codechecker"
 CODECHECKER_SCRIPTS_DIR = f"{CODECHECKER_PATH}/codechecker-common"
 CODECHECKER_BIN_PATH = f"{CODECHECKER_PATH}/build/CodeChecker/bin/"
 CODECHECKER_RESULTCONVERTER_PATH = f"{CODECHECKER_BIN_PATH}report-converter"
+CODECHECKER_MAINSCRIPT_PATH = f"{CODECHECKER_BIN_PATH}/CodeChecker"
 CODECHECKER_SKIPFILE_PATH = f"{HOME}/spa_comparison/C_Cpp/codechecker_skipfile"
 sys.path.extend(CODECHECKER_SCRIPTS_DIR)
 
 
 def store_to_codechecker(result_path, store_project_name):
-    return subprocess.run(["CodeChecker", "store", result_path, "--name",
+    return subprocess.run([CODECHECKER_MAINSCRIPT_PATH, "store", result_path, "--name",
                            store_project_name]).returncode == 0
 
 
