a3a146e74e2757c6aa091081853fa0058ee31af3
Jean Malm <jean.malm@mdh.se>
2021-05-11 12:16:11 +0200

    Added Meson build system to enum, which is used by many GNU projects

--- scripts/build_system_handler.py
+++ scripts/build_system_handler.py
@@ -3,27 +3,32 @@ import os
 from enum import Enum
 
 
-class BuildSystems(Enum):
+# TODO: Would be good to add some generator for build setup and triggering compilation
+# TODO: Refactor this into base- and subclasses instead of enum
+class BuildSystem(Enum):
     UNSUPPORTED = 0,
     Ant = 1,
     Bazel = 2,
     CMake = 3,
     Gradle = 4,
-    Maven = 5
+    Maven = 5,
+    Meson = 6
 
 
 def determine_build_system(base_dir):
     files_in_dir = [f for f in os.listdir(base_dir)]
 
     if "gradlew" in files_in_dir or "gradle" in files_in_dir:
-        return BuildSystems.Gradle
+        return BuildSystem.Gradle
     elif "CMakeLists.txt" in files_in_dir:
-        return BuildSystems.CMake
+        return BuildSystem.CMake
     elif "build.xml" in files_in_dir:
-        return BuildSystems.Ant
+        return BuildSystem.Ant
     elif "pom.xml" in files_in_dir:
-        return BuildSystems.Maven
+        return BuildSystem.Maven
     elif "BUILD.bazel" in files_in_dir:
-        return BuildSystems.Bazel
+        return BuildSystem.Bazel
+    elif "meson.build" in files_in_dir:
+        return BuildSystem.Meson
     else:
-        return BuildSystems.UNSUPPORTED
+        return BuildSystem.UNSUPPORTED
