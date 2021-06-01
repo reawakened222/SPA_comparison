4139fdc95e9ace7cda86705d677ef2eb3d1d0d3d
Jean Malm <jean.malm@mdh.se>
2021-06-01 19:46:47 +0200

    Bugfix in storing wrapper function

--- scripts/codechecker_interface.py
+++ scripts/codechecker_interface.py
@@ -13,10 +13,10 @@ USER = os.getenv("HOME")
 
 CODECHECKER_PATH = f"{USER}/codechecker"
 CODECHECKER_SCRIPTS_DIR = f"{CODECHECKER_PATH}/codechecker-common"
-CODECHECKER_BIN_PATH = f"{CODECHECKER_PATH}/build/CodeChecker/bin/"
+CODECHECKER_BIN_PATH = f"{CODECHECKER_PATH}/build/CodeChecker/bin"
 CODECHECKER_RESULTCONVERTER_PATH = f"{CODECHECKER_BIN_PATH}report-converter"
 CODECHECKER_MAINSCRIPT_PATH = f"{CODECHECKER_BIN_PATH}/CodeChecker"
-CODECHECKER_SKIPFILE_PATH = f"{USER}/spa_comparison/C_Cpp/codechecker_skipfile"
+# CODECHECKER_SKIPFILE_PATH = f"{USER}/spa_comparison/C_Cpp/codechecker_skipfile"
 sys.path.extend(CODECHECKER_SCRIPTS_DIR)
 
 CODECHECKER_SERVER_ADDRESS = os.getenv("CODECHECKER_SERVER_ADDRESS", "localhost:8001")
@@ -28,7 +28,7 @@ def store_to_codechecker(result_path, store_project_name, store_server_product="
     and which product on the server to store results to."""
     return subprocess.run([CODECHECKER_MAINSCRIPT_PATH, "store", result_path,
                            "--name", store_project_name,
-                           "--url", f"{CODECHECKER_SERVER_ADDRESS}/{store_server_product}"]).returncode == 0
+                           "--url", f'{CODECHECKER_SERVER_ADDRESS}/{store_server_product}']).returncode == 0
 
 def gen_convert_to_codechecker_command(analysis_output, analyzer_name, converted_output):
     return [CODECHECKER_RESULTCONVERTER_PATH, "-t",
@@ -41,14 +41,14 @@ def convert_and_store_to_codechecker(analysis_outputpath, codechecker_outputpath
     if res.returncode != 0:
         return False
     return store_to_codechecker(codechecker_outputpath,
-                                f'"{project_name}_{analyzer}{store_name_suffix}"',
+                                f'{project_name}_{analyzer}{store_name_suffix}',
                                 server_product_name)
 
 
 def analysis_post_process(result_folder, tool_name, project_name, server_product_name="Default"):
     """For tools that need to do post-processing on results before submitting to the framework"""
 
-    converted_result_folder = os.path.join(result_folder, tool_name + "_results_converted")
+    converted_result_folder = os.path.join(result_folder, tool_name + '_results_converted')
     return convert_and_store_to_codechecker(result_folder, converted_result_folder, tool_name,
                                             project_name, server_product_name)
 
