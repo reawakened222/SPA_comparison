8ea1d9edff9f732b7f3a7455976f9e04d0ae0a91
Jean Malm <jean.malm@mdh.se>
2021-06-01 19:44:49 +0200

    Started to rework this to include generation of compilation commands etc

--- scripts/build_system_handler.py
+++ scripts/build_system_handler.py
@@ -1,9 +1,11 @@
 import logging
 import os
 from enum import Enum
+from codechecker_interface import CODECHECKER_MAINSCRIPT_PATH
 
 CMAKE_BUILD_DIRECTORY_NAME = "cmakebuild"
 CMAKE_COMPILE_COMMAND_DEFAULT = "compile_commands.json"
+COMPILE_COMMAND_DEFAULT = CMAKE_COMPILE_COMMAND_DEFAULT # In case they may be separated
 
 # TODO: Would be good to add some generator for build setup and triggering compilation
 # TODO: Refactor this into base- and subclasses instead of enum
@@ -18,6 +20,26 @@ class BuildSystem(Enum):
     Maven = 5,
     Meson = 6
 
+def get_bazel_compilecommands(project_path, build_target='//...'):
+    return ['log_bazel_build', project_path, build_target]
+
+# Much of this can likely be a wrapper around CodeChecker's log command
+# https://codechecker.readthedocs.io/en/latest/analyzer/user_guide/#log
+def get_build_commands_compile_database_file(build_system, project_path, build_target=''):
+    """Given a base path to a (fresh) project,
+    this should return the necessary commands
+    for creating a usable compile command file"""
+    if not build_system:
+        return None
+    if not os.path.isdir(project_path):
+        return None
+    path = os.path.abspath(project_path)
+    commands = []
+    compile_command_path = ''
+
+
+
+    return commands, compile_command_path
 
 def determine_build_system(base_dir):
     files_in_dir = [f for f in os.listdir(base_dir)]
