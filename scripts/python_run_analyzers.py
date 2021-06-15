7d6d418a7cdf846d70b22a0f30d785768758de9c
Jean Malm <jean.malm@mdh.se>
2021-06-15 18:58:01 +0200

    python_run_analyzers.py - Updated based on other changes

--- scripts/python_run_analyzers.py
+++ scripts/python_run_analyzers.py
@@ -11,23 +11,14 @@ from datetime import datetime
 # sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 from codechecker_interface import *
 from testware_functions import *
-
+from framework_utils import *
+from compile_command_utils import *
 logging.basicConfig(filename='PYTHON.log', filemode='w', format='%(asctime)s %(message)s')
 LOG = logging.getLogger("PYTHON")
 
-ON_TESTCODE_ONLY = False
 PYTHON_VENV_BASE_NAME = "venv"
 
-
-def filter_compile_command(compile_command_path, filtered_commands_path="compile_commands_filtered.json"):
-    with open(compile_command_path, "r") as data:
-        compile_commands = json.load(data)
-        compile_commands_filtered = list(filter(is_testware_translation_unit, compile_commands))
-        with open(filtered_commands_path, "w+") as outfile:
-            outfile.write(json.dumps(compile_commands_filtered))
-
-
-
+parser = get_framework_args('python')
 
 
 def run_pylama_on_project(outdirpath, project_path, project_name):
@@ -35,10 +26,12 @@ def run_pylama_on_project(outdirpath, project_path, project_name):
     pylama_invocation = ["pylama", "--format", "pylint",
                          "--force", "--report", f"{result_folder}/pylama_results"
                                                 "--abspath", project_path]
-    retcode = subprocess.call(pylama_invocation)  # output folder
-    if retcode == 0:
+    result = time_invocation_log('python', 'pylama', pylama_invocation)  # output folder
+    if result.returncode == 0:
         # We use pylint output format, which framework can already parse
-        analysis_postprocess(result_folder, "pylint", project_name)
+        analysis_post_process(result_folder, "pylint", project_name)
+    else:
+        LOG.error()
 
 
 def run_pyre_on_project(outdir_path, project_path, project_name):
@@ -49,14 +42,13 @@ def run_pyre_on_project(outdir_path, project_path, project_name):
     if venv_dir and len(venv_dir) == 1:
         pyre_invocation.extend(["--search-path", pathlib.Path(venv_dir[0]).absolute()])
     pyre_invocation.extend(["--output", "json", "--noninteractive", "check"])
-    retcode = subprocess.call(pyre_invocation, capture_output=True)
+    result = time_invocation_log('python', 'pyre', pyre_invocation)
 
-    if retcode == 0:
-        analysis_postprocess(result_folder, "pyre", project_name)
-
+    if result.returncode == 0:
+        analysis_post_process(result_folder, "pyre", project_name)
     else:
         LOG.error("Pyre had an error for project " + project_name)
-        LOG.error(str(retcode.stderr))
+        LOG.error(result.stderr.decode('utf-8'))
 
 
 # Assumes that there is a file with compile commands somewhere in the project
@@ -67,23 +59,18 @@ def run_analyzers_on_project(proj_path):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Run analysers')
-    parser.add_argument('--projpath', '-p', help='Project to run script on')
-    parser.add_argument('--single-project', '-sp', help='run toolchain in single project mode', required=False,
-                        default=True)
-    parser.add_argument('--analyse-only-tests', '-at', help='Whether analysis should only be run on tests',
-                        required=False, default=False)
-
     args = parser.parse_args()
-    ON_TESTCODE_ONLY = args.analyse_only_tests
-    os.chdir(args.projpath)
+    if not os.path.isdir(args.path):
+        print(f"Invalid project path {args.path}.")
+        exit(1)
+    os.chdir(args.path)
     # Run tools on all projects
     # 1) Loop through all directories in current working directory
     # 2) get their basename, will be needed for CodeChecker storing later
     # 3) invoke run_* on project
-    if args.single_project:
-        run_analyzers_on_project(os.path.abspath(args.projpath))
+    if not args.recursive:
+        run_analyzers_on_project(os.path.abspath(args.path))
     else:
-        dirs = next(os.walk(args.projpath))[1]
+        dirs = next(os.walk(args.path))[1]
         for d in dirs:
             run_analyzers_on_project(os.path.abspath(d))
